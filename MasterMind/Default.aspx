3e7c66f341596462478287be2faea9204fa015d6
Max Vick <maximum.vick@gmail.com>
2017-11-30 15:28:02 +0000

    Project 1 attempt

---
+++ MasterMind/Default.aspx
@@ -0,0 +1,109 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Mastermind Game</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Mastermind Game</h1>
+       <%-- <asp:Label ID="IntroLabel" runat="server" Text="Try to guess the secret code! Choose your difficulty from the options below: "></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="DifficultyLabel" runat="server" Text="Difficulty:"></asp:Label>
+        <br />
+        <asp:Button ID="EasyButton" runat="server" Text="Easy" />
+        <asp:Label ID="EasyDescription" runat="server" Text="no. colours, no. tries"></asp:Label>
+        <br />
+        <asp:Button ID="NormalButton" runat="server" Text="Normal" />
+        <asp:Label ID="NormalDescription" runat="server" Text="no. colours, no. tries"></asp:Label>
+        <br />
+        <asp:Button ID="HardButton" runat="server" Text="Hard" />
+        <asp:Label ID="HardDescription" runat="server" Text="no. colours, no. tries"></asp:Label>
+        <br /> --%>
+        <asp:Label ID="gameEndLabel" runat="server"></asp:Label>
+        <asp:Label ID="numGen" runat="server" Text="TESTING: This is the correct code:"></asp:Label>
+        <asp:Label ID="numArray" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="testlabel" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="testlabel2" runat="server"></asp:Label>
+        
+
+
+
+    </div>
+    
+    <table runat="server" style="width:300px; height: 300px;">
+       <%-- <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr> --%>
+        <tr>
+            <td>&nbsp;</td>
+            <td><asp:Label ID="R1Slot1" runat="server"></asp:Label></td>
+            <td><asp:Label ID="R1Slot2" runat="server"></asp:Label></td>
+            <td><asp:Label ID="R1Slot3" runat="server"></asp:Label></td>
+            <td><asp:Label ID="R1Slot4" runat="server"></asp:Label></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td><asp:Label ID="guessDisplay1" runat="server"></asp:Label></td>
+            <td><asp:Label ID="guessDisplay2" runat="server"></asp:Label></td>
+            <td><asp:Label ID="guessDisplay3" runat="server"></asp:Label></td>
+            <td><asp:Label ID="guessDisplay4" runat="server"></asp:Label></td>
+        </tr>
+        <tr>
+            <td><asp:Label ID="Label1" runat="server" Text="Label"></asp:Label></td>
+            <td><asp:Button ID="ansButton1" runat="server" Text="1" OnClick="ansButton1_Click1"/></td>
+            <td><asp:Button ID="ansButton2" runat="server" Text="2" OnClick="ansButton2_Click" /></td>
+            <td><asp:Button ID="ansButton3" runat="server" Text="3" OnClick="ansButton3_Click" /></td>
+            <td><asp:Button ID="ansButton4" runat="server" Text="4" OnClick="ansButton4_Click" /></td>
+        </tr>
+    </table>
+    <asp:Button ID="submitButton" runat="server" Text="Submit" OnClick="submitButton_Click" />
+    <asp:Button ID="resetGuessButton" runat="server" Text="Clear Guess" OnClick="resetGuessButton_Click1" />
+    </form>
+</body>
+</html>
---
+++ MasterMind/Default.aspx.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    //initialising the variables needed to randomly generate the final answer the player must guess
+    int Min = 1;
+    int Max = 5;
+    Random codeGenerator = new Random();
+    static int[] codeAnswer = new int[4];
+
+    //initialising the arrays for the players current guesses (which will reset on each new guess) and how many guesses the player has made
+    static int[] currentGuesses = new int[4];
+    static int[] Rounds = new int[8];
+
+    
+   
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        foreach (var item in currentGuesses)
+        {
+            testlabel.Text += item.ToString();
+        }
+    }
+
+    protected void Page_Init(object sender, EventArgs e)
+    {
+
+        // creates the FINAL ANSWER the player must guess on initialising the page
+        for (int i = 0; i < codeAnswer.Length; i++)
+        {
+            codeAnswer[i] = codeGenerator.Next(Min, Max);
+        }
+        foreach (var item in codeAnswer)
+        {
+            numArray.Text += item.ToString();
+        }
+    }
+
+    protected void ansButton1_Click1(object sender, EventArgs e)
+    {
+
+        if (currentGuesses[0] == 0)
+        {
+            guessDisplay1.Text = "1";
+            currentGuesses[0] = 1;
+            testlabel.Text = Convert.ToString(currentGuesses[0]);
+        }
+        else if (currentGuesses[1] == 0)
+        {
+            guessDisplay2.Text = "1";
+            currentGuesses[1] = 1;
+            testlabel.Text = Convert.ToString(currentGuesses[1]);
+        }
+        else if (currentGuesses[2] == 0)
+        {
+            guessDisplay3.Text = "1";
+            currentGuesses[2] = 1;
+            testlabel.Text = Convert.ToString(currentGuesses[2]);
+        }
+        else if (currentGuesses[3] == 0)
+        {
+            guessDisplay4.Text = "1";
+            currentGuesses[3] = 1;
+            testlabel.Text = Convert.ToString(currentGuesses[3]);
+        }
+
+
+    }
+    protected void ansButton2_Click(object sender, EventArgs e)
+    {
+        if (currentGuesses[0] == 0)
+        {
+            guessDisplay1.Text = "2";
+            currentGuesses[0] = 2;
+            testlabel.Text = Convert.ToString(currentGuesses[0]);
+        }
+        else if (currentGuesses[1] == 0)
+        {
+            guessDisplay2.Text = "2";
+            currentGuesses[1] = 2;
+            testlabel.Text = Convert.ToString(currentGuesses[1]);
+        }
+        else if (currentGuesses[2] == 0)
+        {
+            guessDisplay3.Text = "2";
+            currentGuesses[2] = 2;
+            testlabel.Text = Convert.ToString(currentGuesses[2]);
+        }
+        else if (currentGuesses[3] == 0)
+        {
+            guessDisplay4.Text = "2";
+            currentGuesses[3] = 2;
+            testlabel.Text = Convert.ToString(currentGuesses[3]);
+        }
+    }
+    protected void ansButton3_Click(object sender, EventArgs e)
+    {
+        if (currentGuesses[0] == 0)
+        {
+            guessDisplay1.Text = "3";
+            currentGuesses[0] = 3;
+            testlabel.Text = Convert.ToString(currentGuesses[0]);
+        }
+        else if (currentGuesses[1] == 0)
+        {
+            guessDisplay2.Text = "3";
+            currentGuesses[1] = 3;
+            testlabel.Text = Convert.ToString(currentGuesses[1]);
+        }
+        else if (currentGuesses[2] == 0)
+        {
+            guessDisplay3.Text = "3";
+            currentGuesses[2] = 3;
+            testlabel.Text = Convert.ToString(currentGuesses[2]);
+        }
+        else if (currentGuesses[3] == 0)
+        {
+            guessDisplay4.Text = "3";
+            currentGuesses[3] = 3;
+            testlabel.Text = Convert.ToString(currentGuesses[3]);
+        }
+    }
+    protected void ansButton4_Click(object sender, EventArgs e)
+    {
+        if (currentGuesses[0] == 0)
+        {
+            guessDisplay1.Text = "4";
+            currentGuesses[0] = 4;
+            testlabel.Text = Convert.ToString(currentGuesses[0]);
+        }
+        else if (currentGuesses[1] == 0)
+        {
+            guessDisplay2.Text = "4";
+            currentGuesses[1] = 4;
+            testlabel.Text = Convert.ToString(currentGuesses[1]);
+        }
+        else if (currentGuesses[2] == 0)
+        {
+            guessDisplay3.Text = "4";
+            currentGuesses[2] = 4;
+            testlabel.Text = Convert.ToString(currentGuesses[2]);
+        }
+        else if (currentGuesses[3] == 0)
+        {
+            guessDisplay4.Text = "4";
+            currentGuesses[3] = 4;
+            testlabel.Text = Convert.ToString(currentGuesses[3]);
+        }
+    }
+
+    protected void resetGuessButton_Click1(object sender, EventArgs e)
+    {
+        for (int i = 0; i < currentGuesses.Length; i++)
+        {
+            currentGuesses[i] = 0;
+        }
+        guessDisplay1.Text = "";
+        guessDisplay2.Text = "";
+        guessDisplay3.Text = "";
+        guessDisplay4.Text = "";
+    }
+
+
+
+
+    protected void submitButton_Click(object sender, EventArgs e)
+    {
+        int[] submittedGuess = { currentGuesses[0], currentGuesses[1], currentGuesses[2], currentGuesses[3] };
+        string[] guessValidation = new string [4];
+        
+        if (Rounds[0] == 1)
+        {
+            if (submittedGuess == codeAnswer)
+            {
+                gameEndLabel.Text = "You win! Congratulations!";
+            }
+            else if (codeAnswer.Contains(submittedGuess[0]))
+            {
+                guessValidation[0] = "is present";
+
+            }
+
+         /*  string[] printer = { "jupiter", "neptune", "pangea", "mercury", "sonic" };
+            if (printer.Contains("jupiter"))
+            {
+                Process.Start("BLAH BLAH CODE TO ADD PRINTER VIA WINDOWS EXEC"");
+            }
+
+            string[] stringArray = { "text1", "text2", "text3", "text4" };
+            string value = "text3";
+            int pos = Array.IndexOf(stringArray, value);
+            if (pos > -1)*/
+
+
+
+
+
+
+
+        }
+        else
+        {
+
+        }
+        
+    }
+}
+
+
+
